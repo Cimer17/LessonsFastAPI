4aca2590157981e598bca2a6e3339a290efaed97
Cimer17 <ivanklopov2000@yandex.ru>
2025-02-22 07:25:40 +0300

    test

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-# uvicorn main:app --host 0.0.0.0 --port 8000 --reload        
+# uvicorn main:app --host 192.168.0.241 --port 8000 --reload     
+# http://127.0.0.1:8000/docs
 from fastapi import FastAPI, Query
 import random
 
-app = FastAPI()
+app = FastAPI(
+    title="FastAPI Example",
+    description="API с загадками, генератором случайных чисел и шифром Цезаря \nОбщий API КЛЮЧ - mysecretkey123",
+    version="1.0.0"
+)
 
 # Список загадок
 riddles = [
@@ -16,14 +21,12 @@ riddles = [
 # Секретный API-ключ
 SECRET_KEY = "mysecretkey123"
 
-# Функция для выдачи случайной загадки
-@app.get("/riddle")
+@app.get("/riddle", summary="Получить случайную загадку", description="Возвращает случайную загадку с ответом из предустановленного списка.")
 def get_riddle():
     return random.choice(riddles)
 
-# Функция для генерации случайного числа по API-ключу
-@app.get("/random_number")
-def get_random_number(api_key: str = Query(...)):
+@app.get("/random_number", summary="Получить случайное число", description="Возвращает случайное число от 1 до 100, если передан верный API-ключ.")
+def get_random_number(api_key: str = Query(..., description="API-ключ для доступа")):
     if api_key == SECRET_KEY:
         return {"random_number": random.randint(1, 100)}
     else:
@@ -42,17 +45,15 @@ def caesar_cipher(text: str, shift: int, decrypt: bool = False) -> str:
             result += char
     return result
 
-# API для шифрования
-@app.get("/encrypt")
-def encrypt(api_key: str = Query(...), text: str = Query(...), shift: int = Query(...)):
+@app.get("/encrypt", summary="Зашифровать текст методом Цезаря", description="Шифрует текст методом Цезаря с указанным сдвигом, если передан верный API-ключ.")
+def encrypt(api_key: str = Query(..., description="API-ключ для доступа"), text: str = Query(..., description="Текст для шифрования"), shift: int = Query(..., description="Сдвиг для шифра Цезаря")):
     if api_key == SECRET_KEY:
         return {"encrypted_text": caesar_cipher(text, shift)}
     else:
         return {"error": "Неверный API-ключ"}
 
-# API для расшифровки
-@app.get("/decrypt")
-def decrypt(api_key: str = Query(...), text: str = Query(...), shift: int = Query(...)):
+@app.get("/decrypt", summary="Расшифровать текст методом Цезаря", description="Расшифровывает текст методом Цезаря с указанным сдвигом, если передан верный API-ключ.")
+def decrypt(api_key: str = Query(..., description="API-ключ для доступа"), text: str = Query(..., description="Текст для расшифровки"), shift: int = Query(..., description="Сдвиг для шифра Цезаря")):
     if api_key == SECRET_KEY:
         return {"decrypted_text": caesar_cipher(text, shift, decrypt=True)}
     else:
